e002b50848b939841e3d5ca6ab960ea44dff5095
Flavio Oria <flavio.oriap@gmail.com>
2022-10-20 13:10:34 +0200

    Cache añadida a nuestras rutas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 const express = require('express');
+const apicache = require('apicache');
 const initRoutes = require('./v1/routes/index')
+
 
 const PORT = process.env.PORT || 3005;
 
 const app = express()
+
+// Añadiendo cache 
+
+const cache = apicache.middleware;
 
 /* app.get('/', (req, res) => {
     res.send('Hola Mundo');
@@ -11,6 +17,9 @@ const app = express()
 
 //Añadiremos nuestro midleware de json body parser, que nos permite parsear los campo json
 app.use(express.json())
+
+// Estableciendo la cache para todas las rutas
+app.use(cache('5 minutes'))
 
 initRoutes(app);
 
